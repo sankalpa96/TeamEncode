b00b37476bd6a1d77f14a46dfcf5c3ca73f518d1
diasPoornima <poornima.dias89@gmail.com>
2020-08-13 06:23:49 +1000

    Update Book.java
    Changing variable  CALLNO to callNo

--- src/library/entities/Book.java
+++ src/library/entities/Book.java
@@ -6,13 +6,14 @@ import java.io.Serializable;
 // Mediator : Sankalpa
 // Change 1 - change tItLe to title
 // Change 2 - change AuThOr to author
+// Change 3 - change CALLNO to callNo
 
 @SuppressWarnings("serial")
 public class Book implements Serializable {
 	
-	private String title;	//change tItLe to title
+	private String title;	// change tItLe to title
 	private String author;	// change AuThOr to author
-	private String CALLNO;
+	private String callNo;	// change CALLNO to callNo
 	private int iD;
 	
 	private enum sTaTe { AVAILABLE, ON_LOAN, DAMAGED, RESERVED };
@@ -22,8 +23,8 @@ public class Book implements Serializable {
 	public Book(String author, String title, String callNo, int id) {
 		this.author = author;	// change AuThOr to author
 		this.title = title;		// change tItLe to title
-		this.CALLNO = callNo;
-		this.iD = id;
+		this.callNo = callNo;	// change CALLNO to callNo
+		this.iD = id;			
 		this.StAtE = sTaTe.AVAILABLE;
 	}
 	
@@ -32,7 +33,7 @@ public class Book implements Serializable {
 		sb.append("Book: ").append(iD).append("\n")
 		  .append("  Title:  ").append(title).append("\n")		// change tItLe to title
 		  .append("  Author: ").append(author).append("\n")		// change AuThOr to author
-		  .append("  CallNo: ").append(CALLNO).append("\n")
+		  .append("  CallNo: ").append(callNo).append("\n")		// change CALLNO to callNo
 		  .append("  State:  ").append(StAtE);
 		
 		return sb.toString();
