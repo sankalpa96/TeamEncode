d805e80860635fb3792f6241b053389955b70287
diasPoornima <poornima.dias89@gmail.com>
2020-08-15 07:35:09 +1000

    Comment original code
    Add the original code as comments for the modified lines

--- src/library/entities/Book.java
+++ src/library/entities/Book.java
@@ -14,29 +14,45 @@ import java.io.Serializable;
 @SuppressWarnings("serial")
 public class Book implements Serializable {
 	
+	//private String tItLe;
 	private String title;	// change tItLe to title
+	//private String AuThOr;
 	private String author;	// change AuThOr to author
+	//private String CALLNO;
 	private String callNo;	// change CALLNO to callNo
+	//private int iD;
 	private int id;			// change iD to id
 	
+	//private enum sTaTe { AVAILABLE, ON_LOAN, DAMAGED, RESERVED };
 	private enum State { AVAILABLE, ON_LOAN, DAMAGED, RESERVED };	// change sTaTe to State
+	//private sTaTe StAtE;
 	private State state;	// change sTaTe to State and StAtE to state
 	
 	
 	public Book(String author, String title, String callNo, int id) {
+		//this.AuThOr = author;
 		this.author = author;	// change AuThOr to author
+		//this.tItLe = title;
 		this.title = title;		// change tItLe to title
+		//this.CALLNO = callNo;
 		this.callNo = callNo;	// change CALLNO to callNo
+		//this.iD = id;
 		this.id = id;			// change iD to id
+		//this.StAtE = sTaTe.AVAILABLE;
 		this.state = State.AVAILABLE;	// change sTaTe to State and StAtE to state
 	}
 	
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
+		//sb.append("Book: ").append(iD).append("\n")
 		sb.append("Book: ").append(id).append("\n")				// change iD to id
+		//  .append("  Title:  ").append(tItLe).append("\n")
 		  .append("  Title:  ").append(title).append("\n")		// change tItLe to title
+		//  .append("  Author: ").append(AuThOr).append("\n")
 		  .append("  Author: ").append(author).append("\n")		// change AuThOr to author
+		//    .append("  CallNo: ").append(CALLNO).append("\n")
 		  .append("  CallNo: ").append(callNo).append("\n")		// change CALLNO to callNo
+		//  .append("  State:  ").append(StAtE);
 		  .append("  State:  ").append(state);					// change StAtE to state
 		
 		return sb.toString();
@@ -47,31 +63,38 @@ public class Book implements Serializable {
 	}
 
 	public String gEtTiTlE() {
+		//return tItLe;
 		return title;	//change tItLe to title
 	}
 
 
 	
 	public boolean iS_AvAiLaBlE() {
+		//return StAtE == sTaTe.AVAILABLE;
 		return state == State.AVAILABLE;  // change sTaTe to State and StAtE to state
 	}
 
 	
 	public boolean iS_On_LoAn() {
+		//return StAtE == sTaTe.ON_LOAN;
 		return state == State.ON_LOAN;	// change sTaTe to State and StAtE to state
 	}
 
 	
 	public boolean iS_DaMaGeD() {
+		//return StAtE == sTaTe.DAMAGED;
 		return state == State.DAMAGED;	// change sTaTe to State and StAtE to state
 	}
 
 	
 	public void BoRrOw() {
+		//if (StAtE.equals(sTaTe.AVAILABLE)) 
 		if (state.equals(State.AVAILABLE)) 	// change sTaTe to State and StAtE to state
+			//StAtE = sTaTe.ON_LOAN;
 			state = State.ON_LOAN;		// change sTaTe to State and StAtE to state
 		
 		else 
+			//throw new RuntimeException(String.format("Book: cannot borrow while book is in state: %s", StAtE));
 			throw new RuntimeException(String.format("Book: cannot borrow while book is in state: %s", state));  // change StAtE to state
 		
 		
@@ -79,15 +102,19 @@ public class Book implements Serializable {
 
 
 	public void ReTuRn(boolean DaMaGeD) {
+		//if (StAtE.equals(sTaTe.ON_LOAN)) 
 		if (state.equals(State.ON_LOAN)) 	// change sTaTe to State and StAtE to state
 			if (DaMaGeD) 
+				//StAtE = sTaTe.DAMAGED;
 				state = State.DAMAGED;		// change sTaTe to State and StAtE to state
 			
 			else 
+				//StAtE = sTaTe.AVAILABLE;
 				state = State.AVAILABLE;	// change sTaTe to State and StAtE to state
 			
 		
 		else 
+			//throw new RuntimeException(String.format("Book: cannot repair while book is in state: %s", StAtE));
 			throw new RuntimeException(String.format("Book: cannot Return while book is in state: %s", state));  // change StAtE to state
 				
 	}
