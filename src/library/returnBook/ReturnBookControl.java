ea791fdcb2cb95e016c99a2483f6e116e8c21b94
diasPoornima <poornima.dias89@gmail.com>
2020-08-22 07:44:43 +1000

    Change Library and Loan object names
    Change Library and Loan object names lIbRaRy and CurrENT_loan to library and currentLoan respectively

--- src/library/returnBook/ReturnBookControl.java
+++ src/library/returnBook/ReturnBookControl.java
@@ -6,6 +6,7 @@ import library.entities.Loan;
 // Change 1 - change the class name and file name
 // Change 2 - change variable Ui and parameter uI to ui
 // Change 3 - change enum name and enum variable name
+// Change 4 - change Library and Loan object names
 
 // public class rETURN_bOOK_cONTROL{
 public class ReturnBookControl{
@@ -16,13 +17,15 @@ public class ReturnBookControl{
 	private enum ControlState { INITIALISED, READY, INSPECTING };	//change cOnTrOl_sTaTe to ControlState
 	//private cOnTrOl_sTaTe sTaTe;
 	private ControlState state;	//change cOnTrOl_sTaTe to ControlState and sTaTe to state
-	
-	private Library lIbRaRy;
-	private Loan CurrENT_loan;
+	//private Library lIbRaRy;
+	private Library library;	//change lIbRaRy to library
+	//private Loan CurrENT_loan;
+	private Loan currentLoan;	//change CurrENT_loan to currentLoan
 	
 
 	public rETURN_bOOK_cONTROL() {
-		this.lIbRaRy = Library.GeTiNsTaNcE();
+		//this.lIbRaRy = Library.GeTiNsTaNcE();
+		this.library = Library.GeTiNsTaNcE();	//change lIbRaRy to library
 		//sTaTe = cOnTrOl_sTaTe.INITIALISED;
 		state = ControlState.INITIALISED;		//change cOnTrOl_sTaTe to ControlState and sTaTe to state
 	}
@@ -46,7 +49,8 @@ public class ReturnBookControl{
 		if (!state.equals(ControlState.READY)) 	//change cOnTrOl_sTaTe to ControlState and sTaTe to state
 			throw new RuntimeException("ReturnBookControl: cannot call bookScanned except in READY state");
 		
-		Book cUrReNt_bOoK = lIbRaRy.gEt_BoOk(bOoK_iD);
+		//Book cUrReNt_bOoK = lIbRaRy.gEt_BoOk(bOoK_iD);
+		Book cUrReNt_bOoK = library.gEt_BoOk(bOoK_iD);	//change lIbRaRy to library
 		
 		if (cUrReNt_bOoK == null) {
 			//Ui.DiSpLaY("Invalid Book Id");
@@ -57,20 +61,24 @@ public class ReturnBookControl{
 			//Ui.DiSpLaY("Book has not been borrowed");
 			ui.DiSpLaY("Book has not been borrowed");		//change Ui to ui
 			return;
-		}		
-		CurrENT_loan = lIbRaRy.GeT_LoAn_By_BoOkId(bOoK_iD);	
+		}	
+		//CurrENT_loan = lIbRaRy.GeT_LoAn_By_BoOkId(bOoK_iD);			
+		currentLoan = library.GeT_LoAn_By_BoOkId(bOoK_iD);		//change CurrENT_loan, lIbRaRy to currentLoan, library
 		double Over_Due_Fine = 0.0;
-		if (CurrENT_loan.Is_OvEr_DuE()) 
-			Over_Due_Fine = lIbRaRy.CaLcUlAtE_OvEr_DuE_FiNe(CurrENT_loan);
+		//if (CurrENT_loan.Is_OvEr_DuE())
+		if (currentLoan.Is_OvEr_DuE()) 	//change CurrENT_loan to currentLoan
+			//Over_Due_Fine = lIbRaRy.CaLcUlAtE_OvEr_DuE_FiNe(CurrENT_loan);
+			Over_Due_Fine = library.CaLcUlAtE_OvEr_DuE_FiNe(currentLoan);	//change CurrENT_loan, lIbRaRy to currentLoan, library
 		
 		//Ui.DiSpLaY("Inspecting");
 		ui.DiSpLaY("Inspecting");				//change Ui to ui
 		//Ui.DiSpLaY(cUrReNt_bOoK.toString());
 		ui.DiSpLaY(cUrReNt_bOoK.toString());	//change Ui to ui
 		//Ui.DiSpLaY(CurrENT_loan.toString());
-		ui.DiSpLaY(CurrENT_loan.toString());	//change Ui to ui
+		ui.DiSpLaY(currentLoan.toString());	//change Ui, CurrENT_loan to ui, currentLoan
 		
-		if (CurrENT_loan.Is_OvEr_DuE()) 
+		//if (CurrENT_loan.Is_OvEr_DuE()) 
+		if (currentLoan.Is_OvEr_DuE()) 	//change CurrENT_loan to currentLoan
 			//Ui.DiSpLaY(String.format("\nOverdue fine : $%.2f", Over_Due_Fine));
 			ui.DiSpLaY(String.format("\nOverdue fine : $%.2f", Over_Due_Fine));	//change Ui to ui
 		
@@ -96,8 +104,10 @@ public class ReturnBookControl{
 		if (!state.equals(ControlState.INSPECTING)) 	//change cOnTrOl_sTaTe to ControlState and sTaTe to state
 			throw new RuntimeException("ReturnBookControl: cannot call dischargeLoan except in INSPECTING state");
 		
-		lIbRaRy.DiScHaRgE_LoAn(CurrENT_loan, iS_dAmAgEd);
-		CurrENT_loan = null;
+		//lIbRaRy.DiScHaRgE_LoAn(CurrENT_loan, iS_dAmAgEd);
+		library.DiScHaRgE_LoAn(currentLoan, iS_dAmAgEd);		//change lIbRaRy, CurrENT_loan to library, currentLoan
+		//CurrENT_loan = null;
+		currentLoan = null;	//change CurrENT_loan to currentLoan
 		//Ui.sEt_sTaTe(ReturnBookUI.uI_sTaTe.READY);
 		ui.sEt_sTaTe(ReturnBookUI.uI_sTaTe.READY);		//change Ui to ui
 		//sTaTe = cOnTrOl_sTaTe.READY;
