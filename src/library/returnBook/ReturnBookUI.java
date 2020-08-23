f05e293132d508559500c06440eccc09fb9792bf
diasPoornima <poornima.dias89@gmail.com>
2020-08-23 13:35:23 +1000

    Change enum name, enum object name, an object name and a parameter name
    Change enum name uI_sTaTe to UiState and enum object name StATe to state. Also, changed both object name CoNtRoL and parameter name cOnTrOL to control.

--- src/library/returnBook/ReturnBookUI.java
+++ src/library/returnBook/ReturnBookUI.java
@@ -1,21 +1,27 @@
 package library.returnBook;
 import java.util.Scanner;
 
+// Change 1 - change enum name uI_sTaTe and enum object name StATe to UiState and state
+// Change 2 - change object name CoNtRoL and parameter name cOnTrOLto control
 
 public class ReturnBookUI {
+	//public static enum uI_sTaTe { INITIALISED, READY, INSPECTING, COMPLETED };
+	public static enum UiState { INITIALISED, READY, INSPECTING, COMPLETED };	//change uI_sTaTe
+        
+    //private rETURN_bOOK_cONTROL CoNtRoL;
+	private ReturnBookControl control;		//change rETURN_bOOK_cONTROL, cOnTrOL
+	//private uI_sTaTe StATe;
+	private UiState state;		//change uI_sTaTe, StATe
 
-	public static enum uI_sTaTe { INITIALISED, READY, INSPECTING, COMPLETED };
-
-	private rETURN_bOOK_cONTROL CoNtRoL;
-	private Scanner iNpUt;
-	private uI_sTaTe StATe;
-
-	
-	public ReturnBookUI(rETURN_bOOK_cONTROL cOnTrOL) {
-		this.CoNtRoL = cOnTrOL;
+	//public ReturnBookUI(rETURN_bOOK_cONTROL cOnTrOL) {
+	public ReturnBookUI(ReturnBookControl control) {
+        //this.CoNtRoL = cOnTrOL;
+		this.control = control;	//change CoNtRoL, cOnTrOL to control
 		iNpUt = new Scanner(System.in);
-		StATe = uI_sTaTe.INITIALISED;
-		cOnTrOL.sEt_uI(this);
+		//StATe = uI_sTaTe.INITIALISED;
+		state = UiState.INITIALISED;	//change uI_sTaTe, StATe
+		//cOnTrOL.sEt_uI(this);
+		control.sEt_uI(this);	//change CoNtRoL
 	}
 
 
@@ -23,8 +29,8 @@ public class ReturnBookUI {
 		oUtPuT("Return Book Use Case UI\n");
 		
 		while (true) {
-			
-			switch (StATe) {
+			//switch (StATe) {
+			switch (state) {    //change StATe to state
 			
 			case INITIALISED:
 				break;
@@ -32,12 +38,14 @@ public class ReturnBookUI {
 			case READY:
 				String BoOk_InPuT_StRiNg = iNpUt("Scan Book (<enter> completes): ");
 				if (BoOk_InPuT_StRiNg.length() == 0) 
-					CoNtRoL.sCaNnInG_cOmPlEtE();
+					//CoNtRoL.sCaNnInG_cOmPlEtE();
+					control.sCaNnInG_cOmPlEtE();		//change CoNtRoL
 				
 				else {
 					try {
 						int Book_Id = Integer.valueOf(BoOk_InPuT_StRiNg).intValue();
-						CoNtRoL.bOoK_sCaNnEd(Book_Id);
+						//CoNtRoL.bOoK_sCaNnEd(Book_Id);
+						control.bOoK_sCaNnEd(Book_Id);	//change CoNtRoL
 					}
 					catch (NumberFormatException e) {
 						oUtPuT("Invalid bookId");
@@ -51,7 +59,8 @@ public class ReturnBookUI {
 				if (AnS.toUpperCase().equals("Y")) 					
 					Is_DAmAgEd = true;
 				
-				CoNtRoL.dIsChArGe_lOaN(Is_DAmAgEd);
+				//CoNtRoL.dIsChArGe_lOaN(Is_DAmAgEd);
+				control.dIsChArGe_lOaN(Is_DAmAgEd);	//change CoNtRoL
 			
 			case COMPLETED:
 				oUtPuT("Return processing complete");
@@ -59,7 +68,8 @@ public class ReturnBookUI {
 			
 			default:
 				oUtPuT("Unhandled state");
-				throw new RuntimeException("ReturnBookUI : unhandled state :" + StATe);			
+                //throw new RuntimeException("ReturnBookUI : unhandled state :" + StATe);	
+				throw new RuntimeException("ReturnBookUI : unhandled state :" + state);     //change StATe
 			}
 		}
 	}
@@ -80,9 +90,10 @@ public class ReturnBookUI {
 		oUtPuT(object);
 	}
 	
-	public void sEt_sTaTe(uI_sTaTe state) {
-		this.StATe = state;
-	}
+	//public void sEt_sTaTe(uI_sTaTe state) {
+	public void sEt_sTaTe(UiState state) {		//change uI_sTaTe
+		//this.StATe = state;
+		this.state = state;		//change StATe
 
 	
 }
